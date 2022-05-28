0b6bd899a1168546e039020c05564ae58c88c833
phat.itern <phatNfqAsia>
2022-05-28 15:11:19 +0700

    grunt: debug on prod

--- src/backend-feature/context/redisClient.ts
+++ src/backend-feature/context/redisClient.ts
@@ -1,8 +1,13 @@
 import * as redis from "redis";
 
 export async function getRedisClient() {
+  const redisUrl = `redis://${process.env.REDIS_IPADDRESS}:${process.env.REDIS_PORT}`;
+  console.log(
+    "🚀 ~ file: redisClient.ts ~ line 5 ~ getRedisClient ~ redisUrl",
+    redisUrl
+  );
   const redisClient = redis.createClient({
-    url: `redis://${process.env.REDIS_IPADDRESS}:${process.env.REDIS_PORT}`,
+    url: redisUrl,
     password: process.env.REDIS_PASS,
   });
   if (!redisClient.isOpen) {
